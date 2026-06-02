dade89ab290188af2978115388940ceb69238fe8
Rabina-10 <twanabasuroji@gmail.com>
2026-06-02 11:27:46 +0900

    first commit

---
+++ src/component/Contact.jsx
@@ -0,0 +1,10 @@
+function Contact() {
+    return (
+        <>
+        <h2>Contact Information</h2>
+        <p>Tel : 010-6599-8320</p>
+        <p>Email : twanabasuroji@gmail.com</p>
+        </>
+    )
+}
+export default Contact;
